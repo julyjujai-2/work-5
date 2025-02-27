43cea7d329234d442103a97974d8e895313023d1
julyjujai-2 <36629@bpk.ac.th>
2025-02-27 13:46:36 +0700

    Add files via upload

---
+++ login.py
@@ -0,0 +1,6 @@
+username = input("Username: ")
+password = input("Password: ")
+if username == "admin" and password == "Ad13n@23t":
+    print("Welcome, admin")
+else:
+    print("You are not admin")
